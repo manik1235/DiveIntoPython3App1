08682e18f4d1a01c76337aca65a8679b1a2fc9f1
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-10-30 22:39:50 -0400

    Started DiveInChapter6.py

---
+++ DiveIntoPython3App1/Launcher.py
@@ -0,0 +1,7 @@
+'''
+    All files in this solution can be launched from this launcher.
+    Allows for each modification of what script runs.
+'''
+
+if __name__ == "__main__":
+    print ("Success")
