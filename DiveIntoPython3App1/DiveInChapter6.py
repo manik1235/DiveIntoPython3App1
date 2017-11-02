ddb00350cd31fc6de3cdb5659531549e559350e3
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-11-01 21:24:20 -0400

    working on Ch6, Closures. Very difficult to understand

--- DiveIntoPython3App1/DiveInChapter6.py
+++ DiveIntoPython3App1/DiveInChapter6.py
@@ -1,1 +1,48 @@
 
+import re
+
+def build_match_and_apply_functions(pattern, search, replace):
+    def matches_rule(word):
+        return re.search(pattern, word)
+    def apply_rule(word):
+        return re.sub(search, replace, word)
+    return (matches_rule, apply_rule)
+
+h=build_match_and_apply_functions(1,2,3)
+
+
+def match_sxz(noun):
+    return re.search('[sxz]$', noun)
+
+def apply_sxz(noun):
+    return re.sub('$', 'es', noun)
+
+def match_h(noun):
+    return re.search('[^aeioudgkprt]h$', noun)
+
+def apply_h(noun):
+    return re.sub('$', 'es', noun)
+
+def match_y(noun):
+    return re.search('[^aeiou]y$', noun)
+        
+def apply_y(noun):
+    return re.sub('y$', 'ies', noun)
+
+def match_default(noun):
+    return True
+
+def apply_default(noun):
+    return noun + 's'
+
+rules = ((match_sxz, apply_sxz),
+         (match_h, apply_h),
+         (match_y, apply_y),
+         (match_default, apply_default)
+         )
+
+def plural(noun):           
+    for matches_rule, apply_rule in rules:
+        if matches_rule(noun):
+            return apply_rule(noun)
+
